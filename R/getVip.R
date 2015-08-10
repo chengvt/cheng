6a00a2dfce4c92b0fe13da574cccab0c5619318c
chengvt <vipavee.tri@gmail.com>
2015-08-10 14:58:29 +0900

    add getVip

---
+++ R/getVip.R
@@ -0,0 +1,15 @@
+#' @export
+getVip <- function(x) {
+    
+    # extract information
+    if (class(x) == "mvr") {
+        value = t(vip(x))
+        title = "VIP"
+        ncomp = x$ncomp
+    } else {
+        stop('Input class not supported.')
+    }
+    y <- list(title = title, value = value, ncomp = ncomp)
+    class(y) <- "EEMweight"
+    return(y)
+}
