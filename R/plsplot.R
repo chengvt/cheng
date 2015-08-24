5d8581653d55487471f75aec2416da9244b8bf6f
chengvt <vipavee.tri@gmail.com>
2015-08-24 09:22:17 +0900

    add cex

--- R/plsplot.R
+++ R/plsplot.R
@@ -5,27 +5,27 @@ plsplot <- function(model, ncomp = model$ncomp,
                     location = "bottomright",
                     show = c("ncomp", "R2", "RMSE"),
                     round = 2, newx = NULL, newy = NULL,
-                    fitline = TRUE, ...){
+                    fitline = TRUE, cex.stats = 1, ...){
     if (!(is.null(newx) & is.null(newy))) estimate = "test"
     if (estimate %in% "train"){
         predplot(model, which = estimate, ...)
         addstats(model, ncomp = ncomp, 
                  estimate = estimate, location = location,
                  show = show, round = round, newx = newx, 
-                 newy = newy, fitline = fitline)
+                 newy = newy, fitline = fitline, cex = cex.stats)
     } else if (estimate %in% "CV"){
         predplot(model, which = "validation", ...)
         addstats(model, ncomp = ncomp, 
                  estimate = estimate, location = location,
                  show = show, round = round, newx = newx, 
-                 newy = newy, fitline = fitline)
+                 newy = newy, fitline = fitline, cex = cex.stats)
     } else if (estimate %in% "test"){
         predicted <- drop(predict(model, ncomp, newdata = newx))
         plot(newy, predicted, xlab = "measured", ...)
         addstats(model, ncomp = ncomp, 
                  estimate = estimate, location = location,
                  show = show, round = round, newx = newx, 
-                 newy = newy, fitline = fitline)
+                 newy = newy, fitline = fitline, cex = cex.stats)
     }
     
 }
