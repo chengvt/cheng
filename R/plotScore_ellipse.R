9a646fe522be7cfab8a058f2bec4682afac08357
chengvt <vipavee.tri@gmail.com>
2015-11-07 14:11:45 +0900

    debug but really this function should be improved

--- R/plotScore_ellipse.R
+++ R/plotScore_ellipse.R
@@ -1,10 +1,9 @@
+#' @importFrom car dataEllipse
 #' @export
-plotScore_ellipse <- function(PCA, x = 1, y = 2, ...){
+plotScore_ellipse <- function(PCA, x = 1, y = 2, level = 0.95, ...){
     scores <- PCA$x
-    ellipse_matrix <- dataEllipse(scores[,x], scores[,y], level = .997, xlab = prcompname(PCA, x),
-                                  ylab = prcompname(PCA, y), cex = 1.3, draw = F)
-    MIN <- apply(ellipse_matrix, 2, min)
-    MAX <- apply(ellipse_matrix, 2, max)
-    dataEllipse(scores[,x], scores[,y], level = .997, xlab = prcompname(PCA, x),
-                ylab = prcompname(PCA, y), cex = 1.3, xlim = c(MIN[1], MAX[1]), ylim = c(MIN[2], MAX[2]), ...)
+    MIN <- apply(scores[,c(x,y)], 2, min)
+    MAX <- apply(scores[,c(x,y)], 2, max)
+    dataEllipse(scores[,x], scores[,y], level = level, xlab = prcompname(PCA, x),
+                ylab = prcompname(PCA, y), cex.lab = 1.3, xlim = c(MIN[1], MAX[1]), ylim = c(MIN[2], MAX[2]), ...)
 }
