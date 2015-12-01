1c5efecc3fd71988a358627744a57325ce620fa8
chengvt <vipavee.tri@gmail.com>
2015-12-01 10:39:49 +0900

    add documentation

--- R/plsplot.R
+++ R/plsplot.R
@@ -1,6 +1,38 @@
+#' Plot prediction graph for mvr model
+#' 
+#' Plot prediction graph using pls package's `predplot` function but with ability 
+#' to add statistical labeling
+#' 
+#' @param mvr mvr object (pls model)
+#' @param ncomp number of component
+#' @param location legend position
+#' @param show select statistics to show
+#' @param round digits to round
+#' @param newx newx
+#' @param newy newy
+#' @param fitline add fitline or not
+#' @param cex.stats text size of statistics legend
+#' @param ... pass on to predplot
+#' 
+#' @examples 
+#' require(pls)
+#' data(yarn)
+#' model <- plsr(density ~ NIR, 6, data = yarn, validation = "CV")
+#' plsplot(model) # calibration set result
+#' plsplot(model, estimate = "CV") # cross validation set result
+#' 
+#' ## customizing the graphs
+#' plsplot(model, location = "topleft") # change legend position
+#' plsplot(model, show = "R2") # show only R2
+#' plsplot(model, show = "R2", round = 4) # round to four digits
+#' plsplot(model, fitline = FALSE) # get rid of fitline
+#' plsplot(model, show = "R2", cex.stats = 3) # bigger stats font
+#' plsplot(model, cex.lab = 1.5, cex.main = 2) # bigger labels font
+#' 
+#' 
 #' @export
 #' @import pls
-plsplot <- function(model, ncomp = model$ncomp, 
+plsplot <- function(mvr, ncomp = mvr$ncomp, 
                     estimate = "train",
                     location = "bottomright",
                     show = c("ncomp", "R2", "RMSE"),
@@ -8,21 +40,21 @@ plsplot <- function(model, ncomp = model$ncomp,
                     fitline = TRUE, cex.stats = 1, ...){
     if (!(is.null(newx) & is.null(newy))) estimate = "test"
     if (estimate %in% "train"){
-        predplot(model, ncomp = ncomp, which = estimate, ...)
-        addstats(model, ncomp = ncomp, 
+        predplot(mvr, ncomp = ncomp, which = estimate, ...)
+        addstats(mvr, ncomp = ncomp, 
                  estimate = estimate, location = location,
                  show = show, round = round, newx = newx, 
                  newy = newy, fitline = fitline, cex = cex.stats)
     } else if (estimate %in% "CV"){
-        predplot(model, ncomp = ncomp, which = "validation", ...)
-        addstats(model, ncomp = ncomp, 
+        predplot(mvr, ncomp = ncomp, which = "validation", ...)
+        addstats(mvr, ncomp = ncomp, 
                  estimate = estimate, location = location,
                  show = show, round = round, newx = newx, 
                  newy = newy, fitline = fitline, cex = cex.stats)
     } else if (estimate %in% "test"){
-        predicted <- drop(predict(model, ncomp, newdata = newx))
+        predicted <- drop(predict(mvr, ncomp, newdata = newx))
         plot(newy, predicted, ...)
-        addstats(model, ncomp = ncomp, 
+        addstats(mvr, ncomp = ncomp, 
                  estimate = estimate, location = location,
                  show = show, round = round, newx = newx, 
                  newy = newy, fitline = fitline, cex = cex.stats)
--- R/plsplot2.R
+++ R/plsplot2.R
@@ -1,11 +1,46 @@
-#' plot both calibration and validation groups on the same graphs
+#' Plot pls results for both calibration and validation groups
 #' 
-#' As the title says
+#' Plot pls results for both calibration and validation groups on the same graph.
 #' 
-#' @param model mvr object
+#' @param mvr mvr object
+#' @param ncomp number of component
+#' @param location legend position
+#' @param show select statistics to show
+#' @param round digits to round
+#' @param newx newx
+#' @param newy newy
+#' @param fitline add fitline or not
+#' @param cex.stats text size of statistics legend
+#' @param cex.pt point size
+#' @param xlab x-axis label
+#' @param ylab y-axis label
+#' @param col.cal point color for calibration group
+#' @param col.val point color for validation group
+#' @param pch.cal pch for calibration group
+#' @param pch.val pch for validation group
+#' @param ... pass on to plot
+#' 
+#' @examples 
+#' 
+#' require(pls)
+#' data(yarn)
+#' yarn.cal <- yarn[yarn$train,]
+#' yarn.val <- yarn[!yarn$train,]
+#' model <- plsr(density ~ NIR, 15, data = yarn.cal, validation = "CV")
+#' plot_ncomp(model)
+#' ncomp <- 4 # 4 components seem to be appropriate
+#' model <- plsr(density ~ NIR, ncomp, data = yarn.cal) # recalculate
+#' 
+#' plsplot(model) # calibration
+#' plsplot(model, newx = yarn.val$NIR, newy = yarn.val$density) # validation
+#' 
+#' # now put those two plots together
+#' plsplot2(model, newx = yarn.val$NIR, newy = yarn.val$density) # calibration and validation
+#' plsplot2(model, newx = yarn.val$NIR, newy = yarn.val$density, col.cal = "forestgreen", col.val = "skyblue") # change point colors
+#' 
 #' @export
 #' @import pls
-plsplot2 <- function(model, newx = NULL, newy = NULL, ncomp = model$ncomp, 
+plsplot2 <- function(mvr, newx = NULL, newy = NULL, ncomp = mvr$ncomp, 
                      location = "bottomright", show = c("ncomp", "R2", "RMSE"),
                      round = 2, fitline = TRUE, cex.pt = 1.5, cex.stats = 1, 
                      xlab = "Measured value", ylab = "Predicted value",
@@ -13,22 +48,22 @@ plsplot2 <- function(model, newx = NULL, newy = NULL, ncomp = model$ncomp,
                      pch.cal = 21, pch.val = 21, ...){
     
     # get values
-    y.cal.m <- model$model[[1]]
-    y.cal.p <- predict(model, ncomp = ncomp)
+    y.cal.m <- mvr$model[[1]]
+    y.cal.p <- predict(mvr, ncomp = ncomp)
     y.val.m <- newy
-    y.val.p <- drop(predict(model, newdata = newx, ncomp = ncomp))
+    y.val.p <- drop(predict(mvr, newdata = newx, ncomp = ncomp))
     
     # blank plot
     plot(x = c(y.cal.m, y.val.m), y = c(y.cal.p, y.val.p), type = "n", xlab = xlab, ylab = ylab, ...)
     
     # calibration
     points(y.cal.m, y.cal.p, pch = pch.cal, col = "black", bg = col.cal, cex = cex.pt)
-
+    
     # validation
     points(y.val.m, y.val.p, pch = pch.val, col = "black", bg = col.val, cex = cex.pt)
     
     # add stats
-    addstats(model, ncomp = ncomp, 
+    addstats(mvr, ncomp = ncomp, 
              estimate = "test", location = location,
              show = show, round = round, newx = newx, 
              newy = newy, fitline = fitline, cex = cex.stats)
@@ -37,4 +72,4 @@ plsplot2 <- function(model, newx = NULL, newy = NULL, ncomp = model$ncomp,
     legend("topleft", legend = c("Calibration", "Validation"), 
            pch = c(pch.cal, pch.val), col = "black", pt.bg = c(col.cal, col.val), pt.cex = cex.pt)
     
-    }
+}
