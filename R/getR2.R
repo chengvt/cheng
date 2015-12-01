6b3989837efd729e97e371f67b85543ec240d977
chengvt <vipavee.tri@gmail.com>
2015-12-01 09:48:55 +0900

    add documentation

--- R/getR2.R
+++ R/getR2.R
@@ -1,20 +1,41 @@
-#' get R2
+#' get R2 from mvr object
 #' 
-#' get R2
+#' Alternative to pls package's `R2`. While `R2` requires a `newdata` dataframe which combines
+#' both predictors and target, `getR2` lets you put in `newx` and `newy` separately. 
+#' Aside from that, declaring `estimate` in `getR2` remind you which value you got. 
 #' 
 #' @param mvr mvr object
+#' @param ncomp number of component
 #' @param estimate a subset of c("train", "CV", "test")
 #' @param newx predictors for test group
 #' @param newy measured value for test group
+#' @param showprint show result in console
+#' 
+#' @examples 
+#' require(pls)
+#' data(yarn)
+#' model <- plsr(density ~ NIR, 6, data = yarn, validation = "CV")
+#' R2(model)
+#' getR2(model, estimate = "train") # return R2 at particular ncomp without intercept value
+#' getR2(model, estimate = "CV")
 #' 
 #' @export
-getR2 <- function(mvr, ncomp = mvr$ncomp, estimate, newx = NULL, newy = NULL){
+getR2 <- function(mvr, ncomp = mvr$ncomp, estimate, newx = NULL, newy = NULL, showprint = TRUE){
     if (estimate %in% "test"){
         predicted <- drop(predict(mvr, ncomp, newdata = newx))
         fit <- lm(predicted ~ newy)
         R2 <- summary(fit)$r.squared
     } else {
         R2 <- R2(mvr, estimate = estimate)$val[1,1,ncomp+1]   
+    }
+    
+    if (showprint){
+        output <- "R2"
+        if (estimate %in% "train") output <- paste0(output, "C")
+        if (estimate %in% "CV") output <- paste0(output, "CV")
+        if (estimate %in% "test") output <- paste0(output, "P")
+        cat(paste0(output, " = ", round(R2, 4), " (ncomp = ", ncomp, ")"))
+        cat("\n")
     }
     return(R2)
 }
--- R/getRMSE.R
+++ R/getRMSE.R
@@ -1,19 +1,41 @@
-#' get RMSE
+#' get RMSE from mvr object
 #' 
-#' get RMSE
+#' Alternative to pls package's `RMSEP`. While `RMSEP` requires a `newdata` dataframe which combines
+#' both predictors and target, `getRMSE` lets you put in `newx` and `newy` separately. 
+#' Aside from that, declaring `estimate` in `getRMSE` remind you which value you got. 
 #' 
 #' @param mvr mvr object
+#' @param ncomp number of component
 #' @param estimate a subset of c("train", "CV", "test")
 #' @param newx predictors for test group
 #' @param newy measured value for test group
+#' @param showprint show result in console
+#' 
+#' @examples 
+#' require(pls)
+#' data(yarn)
+#' model <- plsr(density ~ NIR, 6, data = yarn, validation = "CV")
+#' RMSEP(model)
+#' getRMSE(model, estimate = "train") # return RMSE at particular ncomp without intercept value
+#' getRMSE(model, estimate = "CV")
 #' 
 #' @export
-getRMSE <- function(mvr, ncomp = mvr$ncomp, estimate, newx = NULL, newy = NULL){
+getRMSE <- function(mvr, ncomp = mvr$ncomp, estimate, newx = NULL, newy = NULL, showprint = TRUE){
+    if ((!is.null(newx)) & (!is.null(newy))) estimate <- "test"
     if (estimate %in% "test"){
         predicted <- drop(predict(mvr, ncomp, newdata = newx))
         RMSE <- sqrt(sum((predicted - newy) ^ 2) / length(predicted))
     } else {
         RMSE <- RMSEP(mvr, estimate = estimate)$val[1,1,ncomp+1]
+    }
+    
+    if (showprint){
+        output <- "RMSE"
+        if (estimate %in% "train") output <- paste0(output, "C")
+        if (estimate %in% "CV") output <- paste0(output, "CV")
+        if (estimate %in% "test") output <- paste0(output, "P")
+        cat(paste0(output, " = ", round(RMSE, 4), " (ncomp = ", ncomp, ")"))
+        cat("\n")
     }
     return(RMSE)
 }
