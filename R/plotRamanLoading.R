924db7dfefdbec08e18e711984d1c33b4d002c0a
chengvt <vipavee.tri@gmail.com>
2015-12-10 09:54:48 +0900

    add customization of xlab

--- R/plotRamanLoading.R
+++ R/plotRamanLoading.R
@@ -8,12 +8,12 @@
 #' @param ... additional arguments for \code{\link[graphic]{matplot}}
 #' 
 #' @export
-plotRamanLoading <- function(PCA, ncomp = 1:6, ylab = "loading", ...){
+plotRamanLoading <- function(PCA, ncomp = 1:6, xlab = "Raman shift (1/cm)", ylab = "loading", ...){
     par(mar = c(5,4,4,5) + 0.1)
     loading <- PCA$rotation
     wavelength <- rownames(loading)
     matplot(wavelength, loading[, ncomp], lty = 1, type = "l", 
-            ylab = ylab, ...)
+            xlab = xlab, ylab = ylab, ...)
     par(xpd = T)
     legend(par()$usr[2], par()$usr[4], legend = ncomp, lty = 1, col = ncomp)
     par(mar = c(5,4,4,2) + 0.1)
