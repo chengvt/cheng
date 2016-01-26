e5b0f23344178d83f8086bc90277c0f6be1f932b
chengvt <vipavee.tri@gmail.com>
2016-01-26 16:43:18 +0900

    debug

--- R/inflate.R
+++ R/inflate.R
@@ -3,7 +3,7 @@ inflate <- function(x, n){
     if (is.matrix(x)) {
         y <- apply(x, 2, function(x) rep(x, each = n))
     } else if (is.data.frame(x)) {
-        y <- apply(x, 2, function(x) rep(x, each = n))
+        y <- as.data.frame(apply(x, 2, function(x) rep(x, each = n)))
     } else break
     return(y)
 }
