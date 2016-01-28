3d4d64d910712146118e8a454e102bf7d113eabc
chengvt <vipavee.tri@gmail.com>
2016-01-28 16:29:29 +0900

    add inflate.R

--- R/inflate.R
+++ R/inflate.R
@@ -4,6 +4,11 @@ inflate <- function(x, n){
         y <- apply(x, 2, function(x) rep(x, each = n))
     } else if (is.data.frame(x)) {
         y <- as.data.frame(apply(x, 2, function(x) rep(x, each = n)), stringsAsFactors = FALSE)
+        
+        # copy class if different
+        if (!isTRUE(base::all.equal(sapply(x, class), sapply(y, class)))){
+            y <- as.data.frame(mapply(FUN = as, y, sapply(x, class), SIMPLIFY = FALSE))
+        }
     } else break
     return(y)
 }
